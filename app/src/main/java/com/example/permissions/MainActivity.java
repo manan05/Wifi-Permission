df0770669e435734cba5c9e2deb063c13664b56b
Manan Arora <31929934+manan0313@users.noreply.github.com>
2019-07-30 02:31:45 +0530

    Updated

---
+++ app/src/main/java/com/example/permissions/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.permissions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnStatusBar;
+    TextView tvStatus;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnStatusBar = findViewById(R.id.btnStatusBar);
+        tvStatus = findViewById(R.id.tvStatus);
+
+        btnStatusBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ConnectivityManager cm =
+                        (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+                NetworkInfo netInfo = cm.getActiveNetworkInfo();
+                boolean isConnected = netInfo != null &&
+                        netInfo.isConnected();
+                tvStatus.setText(isConnected? "CONNECTED":"DISCONNECTED");
+            }
+        });
+    }
+}
